bfb0e44050d749a49319d8e3d6effabeace35a9d
Garin Prakoso <gerin.rin@gmail.com>
2020-07-08 13:02:14 +0700

    add validation min max password and username

--- domains/user/model.go
+++ domains/user/model.go
@@ -3,8 +3,8 @@ package user
 // User Struct
 type User struct {
 	ID       string `json:"id"`
-	Username string `json:"username" validate:"nonzero"`
-	Password string `json:"password" validate:"nonzero"`
+	Username string `json:"username" validate:"nonzero, min=4, max=16"`
+	Password string `json:"password" validate:"nonzero, min=4, max=16"`
 	Status   string `json:"status"`
 	Created  string `json:"created"`
 	Updated  string `json:"updated"`
